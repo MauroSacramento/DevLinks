8de34bd736d8b1b12e757a1d662b49617d1d4d2e
Mauro <maurosacramento35@gmail.com>
2023-09-29 13:41:41 +0100

    add readme

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function toggleMode(){
+function toggleMode() {
   const html = document.documentElement
   html.classList.toggle('light')
 
   // Pegar a imagem
   const img = document.querySelector("#profile img")
   //Substituir segundo a condição
-  if (html.classList.contains('light')){
-  img.setAttribute('src', './assets/avatar-light.png')
-  }else{
+  if (html.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
     img.setAttribute('src', './assets/avatar.png')
   }
 }
